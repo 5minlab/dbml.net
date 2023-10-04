d1526c5f90020045d037179d4f3aff50c085baab
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-04 22:40:38 +0300

    - PASS: `Parse_CompositeIndexDeclaration_With_Two_Indexes_Empty_Settings`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.CompositeIndexDeclaration.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.CompositeIndexDeclaration.cs
@@ -137,4 +137,30 @@ public partial class ParserTests
         e.AssertToken(secondIndexNameKind, secondIndexNameText);
         e.AssertToken(SyntaxKind.CloseParenthesisToken, ")");
     }
+
+    [Fact]
+    public void Parse_CompositeIndexDeclaration_With_Two_Indexes_Empty_Settings()
+    {
+        SyntaxKind firstIndexNameKind = SyntaxKind.IdentifierToken;
+        string firstIndexNameText = CreateRandomString();
+        SyntaxKind secondIndexNameKind = SyntaxKind.IdentifierToken;
+        string secondIndexNameText = CreateRandomString();
+        string indexText = $"( {firstIndexNameText}, {secondIndexNameText}) [ ]";
+        string text = "indexes { " + indexText + " }";
+
+        CompositeIndexDeclarationSyntax compositeFieldIndexDeclarationSyntax =
+            ParseCompositeIndexDeclaration(text);
+
+        using AssertingEnumerator e = new AssertingEnumerator(compositeFieldIndexDeclarationSyntax);
+        e.AssertNode(SyntaxKind.CompositeIndexDeclarationStatement);
+        e.AssertToken(SyntaxKind.OpenParenthesisToken, "(");
+        e.AssertNode(SyntaxKind.NameExpression);
+        e.AssertToken(firstIndexNameKind, firstIndexNameText);
+        e.AssertToken(SyntaxKind.CommaToken, ",");
+        e.AssertNode(SyntaxKind.NameExpression);
+        e.AssertToken(secondIndexNameKind, secondIndexNameText);
+        e.AssertToken(SyntaxKind.CloseParenthesisToken, ")");
+        e.AssertToken(SyntaxKind.OpenBracketToken, "[");
+        e.AssertToken(SyntaxKind.CloseBracketToken, "]");
+    }
 }
