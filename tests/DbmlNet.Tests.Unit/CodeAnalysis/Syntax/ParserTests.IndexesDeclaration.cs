f582de61c93d4b2155d750df8ac8236e86e27dd3
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-04 21:52:05 +0300

    - PASS: `Parse_IndexesDeclaration_With_Index_Identifier`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.IndexesDeclaration.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.IndexesDeclaration.cs
@@ -19,4 +19,25 @@ public partial class ParserTests
         e.AssertToken(SyntaxKind.OpenBraceToken, "{");
         e.AssertToken(SyntaxKind.CloseBraceToken, "}");
     }
+
+    [Fact]
+    public void Parse_IndexesDeclaration_With_Index_Identifier()
+    {
+        SyntaxKind indexNameKind = SyntaxKind.IdentifierToken;
+        string randomText = CreateRandomString();
+        string indexNameText = randomText;
+        object? indexNameValue = null;
+        string indexText = $"{indexNameText}";
+        string text = "indexes { " + indexText + " }";
+
+        StatementSyntax statement = ParseStatement(text);
+
+        using AssertingEnumerator e = new AssertingEnumerator(statement);
+        e.AssertNode(SyntaxKind.IndexesDeclarationStatement);
+        e.AssertToken(SyntaxKind.IndexesKeyword, "indexes");
+        e.AssertToken(SyntaxKind.OpenBraceToken, "{");
+        e.AssertNode(SyntaxKind.SingleFieldIndexDeclarationStatement);
+        e.AssertToken(indexNameKind, indexNameText, indexNameValue);
+        e.AssertToken(SyntaxKind.CloseBraceToken, "}");
+    }
 }
