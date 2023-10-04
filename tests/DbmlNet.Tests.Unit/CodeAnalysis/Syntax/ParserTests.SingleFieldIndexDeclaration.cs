28f655a9e8e13ccd37a9a67165e96091af94ee34
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-04 21:51:43 +0300

    - Move `SingleFieldIndexDeclaration` tests

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.SingleFieldIndexDeclaration.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.SingleFieldIndexDeclaration.cs
@@ -7,6 +7,74 @@ namespace DbmlNet.Tests.Unit.CodeAnalysis.Syntax;
 public partial class ParserTests
 {
     [Fact]
+    public void Parse_SingleFieldIndexDeclaration_With_Identifier_Name()
+    {
+        SyntaxKind indexNameKind = SyntaxKind.IdentifierToken;
+        string randomText = CreateRandomString();
+        string indexNameText = randomText;
+        object? indexNameValue = null;
+        string indexText = $"{indexNameText}";
+        string text = "indexes { " + indexText + " }";
+
+        SingleFieldIndexDeclarationSyntax singleFieldIndexDeclarationSyntax =
+            ParseSingleFieldIndexDeclaration(text);
+
+        using AssertingEnumerator e = new AssertingEnumerator(singleFieldIndexDeclarationSyntax);
+        e.AssertNode(SyntaxKind.SingleFieldIndexDeclarationStatement);
+        e.AssertToken(indexNameKind, indexNameText, indexNameValue);
+    }
+
+    [Fact(Skip = "Skip to avoid infinite loop.")]
+    public void Parse_SingleFieldIndexDeclaration_With_QuotationMarksString_Name()
+    {
+        SyntaxKind indexNameKind = SyntaxKind.QuotationMarksStringToken;
+        string randomText = CreateRandomMultiWordString();
+        string indexNameText = $"\"{randomText}\"";
+        object? indexNameValue = randomText;
+        string indexText = $"{indexNameText}";
+        string text = "indexes {" + indexText + "}";
+
+        StatementSyntax statement = ParseStatement(text);
+
+        IndexesDeclarationSyntax indexesDeclarationSyntax =
+            Assert.IsAssignableFrom<IndexesDeclarationSyntax>(statement);
+
+        SingleFieldIndexDeclarationSyntax singleFieldIndexDeclarationSyntax =
+            Assert.IsAssignableFrom<SingleFieldIndexDeclarationSyntax>(
+                Assert.Single(indexesDeclarationSyntax.Indexes)
+            );
+
+        using AssertingEnumerator e = new AssertingEnumerator(singleFieldIndexDeclarationSyntax);
+        e.AssertNode(SyntaxKind.SingleFieldIndexDeclarationStatement);
+        e.AssertToken(indexNameKind, indexNameText, indexNameValue);
+    }
+
+    [Fact(Skip = "Skip to avoid infinite loop.")]
+    public void Parse_SingleFieldIndexDeclaration_With_SingleQuotationMarksString_Name()
+    {
+        SyntaxKind indexNameKind = SyntaxKind.SingleQuotationMarksStringToken;
+        string randomText = CreateRandomMultiWordString();
+        string indexNameText = $"\'{randomText}\'";
+        object? indexNameValue = randomText;
+        string indexText = $"{indexNameText}";
+        string text = "indexes {" + indexText + "}";
+
+        StatementSyntax statement = ParseStatement(text);
+
+        IndexesDeclarationSyntax indexesDeclarationSyntax =
+            Assert.IsAssignableFrom<IndexesDeclarationSyntax>(statement);
+
+        SingleFieldIndexDeclarationSyntax singleFieldIndexDeclarationSyntax =
+            Assert.IsAssignableFrom<SingleFieldIndexDeclarationSyntax>(
+                Assert.Single(indexesDeclarationSyntax.Indexes)
+            );
+
+        using AssertingEnumerator e = new AssertingEnumerator(singleFieldIndexDeclarationSyntax);
+        e.AssertNode(SyntaxKind.SingleFieldIndexDeclarationStatement);
+        e.AssertToken(indexNameKind, indexNameText, indexNameValue);
+    }
+
+    [Fact]
     public void Parse_SingleFieldIndexDeclaration_With_Identifier_Name_And_No_Settings()
     {
         SyntaxKind indexNameKind = SyntaxKind.IdentifierToken;
