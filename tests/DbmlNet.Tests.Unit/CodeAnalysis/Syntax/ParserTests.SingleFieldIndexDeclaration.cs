12f7e0eb1a0ed4a51e904b80cff3f5ef74d90e76
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-04 21:32:20 +0300

    - PASS: `Parse_SingleFieldIndexDeclaration_With_Identifier_Name_And_Empty_Settings`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.SingleFieldIndexDeclaration.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.SingleFieldIndexDeclaration.cs
@@ -23,4 +23,24 @@ public partial class ParserTests
         e.AssertNode(SyntaxKind.SingleFieldIndexDeclarationStatement);
         e.AssertToken(indexNameKind, indexNameText, indexNameValue);
     }
+
+    [Fact]
+    public void Parse_SingleFieldIndexDeclaration_With_Identifier_Name_And_Empty_Settings()
+    {
+        SyntaxKind indexNameKind = SyntaxKind.IdentifierToken;
+        string randomText = CreateRandomString();
+        string indexNameText = randomText;
+        object? indexNameValue = null;
+        string indexText = $"{indexNameText} [ ]";
+        string text = "indexes { " + indexText + " }";
+
+        SingleFieldIndexDeclarationSyntax singleFieldIndexDeclarationSyntax =
+            ParseSingleFieldIndexDeclaration(text);
+
+        using AssertingEnumerator e = new AssertingEnumerator(singleFieldIndexDeclarationSyntax);
+        e.AssertNode(SyntaxKind.SingleFieldIndexDeclarationStatement);
+        e.AssertToken(indexNameKind, indexNameText, indexNameValue);
+        e.AssertToken(SyntaxKind.OpenBracketToken, "[");
+        e.AssertToken(SyntaxKind.CloseBracketToken, "]");
+    }
 }
