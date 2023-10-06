b1358d4101bd2df740dd906d61e600201ae8a452
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-07 01:54:03 +0300

    - PASS: `Parse_SingleFieldIndexDeclaration_With_Unknown_KeyValue_Setting_Identifier_Value`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.SingleFieldIndexDeclaration.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.SingleFieldIndexDeclaration.cs
@@ -452,4 +452,35 @@ public partial class ParserTests
         e.AssertToken(settingNameKind, settingNameText, settingName);
         e.AssertToken(SyntaxKind.CloseBracketToken, "]");
     }
+
+    [Fact]
+    public void Parse_SingleFieldIndexDeclaration_With_Unknown_KeyValue_Setting_Identifier_Value()
+    {
+        SyntaxKind indexNameKind = SyntaxKind.IdentifierToken;
+        string randomText = CreateRandomString();
+        string indexNameText = randomText;
+        object? indexNameValue = null;
+        SyntaxKind settingNameKind = SyntaxKind.IdentifierToken;
+        string settingNameText = CreateRandomString();
+        object? settingName = null;
+        SyntaxKind settingValueKind = SyntaxKind.IdentifierToken;
+        string settingValueText = CreateRandomString();
+        object? settingValue = null;
+        string indexText = $"{indexNameText} [ {settingNameText}: {settingValueText} ]";
+        string text = "indexes { " + indexText + " }";
+
+        SingleFieldIndexDeclarationSyntax singleFieldIndexDeclarationSyntax =
+            ParseSingleFieldIndexDeclaration(text);
+
+        using AssertingEnumerator e = new AssertingEnumerator(singleFieldIndexDeclarationSyntax);
+        e.AssertNode(SyntaxKind.SingleFieldIndexDeclarationStatement);
+        e.AssertToken(indexNameKind, indexNameText, indexNameValue);
+        e.AssertNode(SyntaxKind.IndexSettingListClause);
+        e.AssertToken(SyntaxKind.OpenBracketToken, "[");
+        e.AssertNode(SyntaxKind.UnknownIndexSettingClause);
+        e.AssertToken(settingNameKind, settingNameText, settingName);
+        e.AssertToken(SyntaxKind.ColonToken, ":");
+        e.AssertToken(settingValueKind, settingValueText, settingValue);
+        e.AssertToken(SyntaxKind.CloseBracketToken, "]");
+    }
 }
