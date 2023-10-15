7d261ae10874d33f14926729e3ea941a91357f7c
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-15 19:49:11 +0300

    - PASS: `Parse_CallExpression_With_SingleQuotationMarksString_Argument`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.CallExpression.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.CallExpression.cs
@@ -108,4 +108,26 @@ public partial class ParserTests
         e.AssertToken(argKind, argText, argValue);
         e.AssertToken(SyntaxKind.CloseParenthesisToken, ")");
     }
+
+    [Fact]
+    public void Parse_CallExpression_With_SingleQuotationMarksString_Argument()
+    {
+        SyntaxKind functionNameKind = SyntaxKind.IdentifierToken;
+        string functionNameText = CreateRandomString();
+        SyntaxKind argKind = SyntaxKind.SingleQuotationMarksStringToken;
+        string argRandomValue = CreateRandomMultiWordString();
+        string argText = $"\'{argRandomValue}\'";
+        object? argValue = argRandomValue;
+        string text = $"{functionNameText} ( {argText} ) ";
+
+        ExpressionSyntax expression = ParseExpression(text);
+
+        using AssertingEnumerator e = new AssertingEnumerator(expression);
+        e.AssertNode(SyntaxKind.CallExpression);
+        e.AssertToken(functionNameKind, functionNameText);
+        e.AssertToken(SyntaxKind.OpenParenthesisToken, "(");
+        e.AssertNode(SyntaxKind.LiteralExpression);
+        e.AssertToken(argKind, argText, argValue);
+        e.AssertToken(SyntaxKind.CloseParenthesisToken, ")");
+    }
 }
