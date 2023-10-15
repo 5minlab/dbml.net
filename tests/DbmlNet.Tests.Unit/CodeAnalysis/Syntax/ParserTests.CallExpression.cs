53799475ea30df403cd879a45650d5188e31031d
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-15 19:40:49 +0300

    - PASS: `Parse_CallExpression_With_NumberLiteral_Argument`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.CallExpression.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.CallExpression.cs
@@ -22,4 +22,26 @@ public partial class ParserTests
         e.AssertToken(SyntaxKind.OpenParenthesisToken, "(");
         e.AssertToken(SyntaxKind.CloseParenthesisToken, ")");
     }
+
+    [Fact]
+    public void Parse_CallExpression_With_NumberLiteral_Argument()
+    {
+        SyntaxKind functionNameKind = SyntaxKind.IdentifierToken;
+        string functionNameText = CreateRandomString();
+        SyntaxKind argKind = SyntaxKind.NumberToken;
+        decimal argRandomNumber = GetRandomNumber();
+        string argText = $"{argRandomNumber}";
+        object? argValue = argRandomNumber;
+        string text = $"{functionNameText} ( {argText} ) ";
+
+        ExpressionSyntax expression = ParseExpression(text);
+
+        using AssertingEnumerator e = new AssertingEnumerator(expression);
+        e.AssertNode(SyntaxKind.CallExpression);
+        e.AssertToken(functionNameKind, functionNameText);
+        e.AssertToken(SyntaxKind.OpenParenthesisToken, "(");
+        e.AssertNode(SyntaxKind.LiteralExpression);
+        e.AssertToken(argKind, argText, argValue);
+        e.AssertToken(SyntaxKind.CloseParenthesisToken, ")");
+    }
 }
