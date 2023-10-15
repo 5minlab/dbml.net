49aaed2cc8934f9aeefc8983ab0294dfcc148a7b
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-15 06:59:25 +0300

    - PASS: `Parse_TableDeclaration_With_Note_QuotationMarksString`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.TableDeclaration.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.TableDeclaration.cs
@@ -85,4 +85,32 @@ public partial class ParserTests
         e.AssertToken(SyntaxKind.OpenBraceToken, "{");
         e.AssertToken(SyntaxKind.CloseBraceToken, "}");
     }
+
+    [Fact]
+    public void Parse_TableDeclaration_With_Note_QuotationMarksString()
+    {
+        SyntaxKind tableNameKind = SyntaxKind.IdentifierToken;
+        string randomText = CreateRandomString();
+        string tableNameText = randomText;
+        object? tableNameValue = null;
+        SyntaxKind noteValueKind = SyntaxKind.QuotationMarksStringToken;
+        string randomNoteText = CreateRandomMultiWordString();
+        string noteValueText = $"\"{randomNoteText}\"";
+        object? noteValue = randomNoteText;
+        string text = $"Table {tableNameText} {{ note: {noteValueText} }}";
+
+        MemberSyntax member = ParseMember(text);
+
+        using AssertingEnumerator e = new AssertingEnumerator(member);
+        e.AssertNode(SyntaxKind.TableDeclarationMember);
+        e.AssertToken(SyntaxKind.TableKeyword, "Table");
+        e.AssertToken(tableNameKind, tableNameText, tableNameValue);
+        e.AssertNode(SyntaxKind.BlockStatement);
+        e.AssertToken(SyntaxKind.OpenBraceToken, "{");
+        e.AssertNode(SyntaxKind.NoteDeclarationStatement);
+        e.AssertToken(SyntaxKind.NoteKeyword, "note");
+        e.AssertToken(SyntaxKind.ColonToken, ":");
+        e.AssertToken(noteValueKind, noteValueText, noteValue);
+        e.AssertToken(SyntaxKind.CloseBraceToken, "}");
+    }
 }
