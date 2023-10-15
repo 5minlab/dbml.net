8b68a132f457aea34292b8a3fdefa3eef3640fe3
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-15 19:43:26 +0300

    - PASS: `Parse_CallExpression_With_False_BooleanLiteral_Argument`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.CallExpression.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.CallExpression.cs
@@ -24,6 +24,27 @@ public partial class ParserTests
     }
 
     [Fact]
+    public void Parse_CallExpression_With_False_BooleanLiteral_Argument()
+    {
+        SyntaxKind functionNameKind = SyntaxKind.IdentifierToken;
+        string functionNameText = CreateRandomString();
+        SyntaxKind argKind = SyntaxKind.FalseKeyword;
+        string argText = "false";
+        object? argValue = false;
+        string text = $"{functionNameText} ( {argText} ) ";
+
+        ExpressionSyntax expression = ParseExpression(text);
+
+        using AssertingEnumerator e = new AssertingEnumerator(expression);
+        e.AssertNode(SyntaxKind.CallExpression);
+        e.AssertToken(functionNameKind, functionNameText);
+        e.AssertToken(SyntaxKind.OpenParenthesisToken, "(");
+        e.AssertNode(SyntaxKind.LiteralExpression);
+        e.AssertToken(argKind, argText, argValue);
+        e.AssertToken(SyntaxKind.CloseParenthesisToken, ")");
+    }
+
+    [Fact]
     public void Parse_CallExpression_With_NumberLiteral_Argument()
     {
         SyntaxKind functionNameKind = SyntaxKind.IdentifierToken;
