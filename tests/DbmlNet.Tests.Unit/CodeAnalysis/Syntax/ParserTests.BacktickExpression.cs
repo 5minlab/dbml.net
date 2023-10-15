60288e1431a3447c1952e5c33dc9dd48e0dd59a2
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-15 20:41:15 +0300

    - PASS: `Parse_BacktickExpression_With_LiteralExpression`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.BacktickExpression.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.BacktickExpression.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 using DbmlNet.CodeAnalysis.Syntax;
 
 using Xunit;
@@ -6,6 +8,33 @@ namespace DbmlNet.Tests.Unit.CodeAnalysis.Syntax;
 
 public partial class ParserTests
 {
+    [Theory]
+    [MemberData(nameof(GetLiteralTokensData))]
+    public void Parse_BacktickExpression_With_LiteralExpression(
+        SyntaxKind literalKind, string literalText, object? literalValue)
+    {
+        string text = $"`{literalText}`";
+
+        BacktickExpressionSyntax expression = ParseBacktickExpression(text);
+
+        using AssertingEnumerator e = new AssertingEnumerator(expression.Expression);
+        e.AssertNode(SyntaxKind.LiteralExpression);
+        e.AssertToken(literalKind, literalText, literalValue);
+    }
+
+    public static IEnumerable<object?[]> GetLiteralTokensData()
+    {
+        yield return new object?[] { SyntaxKind.TrueKeyword, "true", true };
+        yield return new object?[] { SyntaxKind.FalseKeyword, "false", false };
+        yield return new object?[] { SyntaxKind.NumberToken, "1", 1m };
+        yield return new object?[] { SyntaxKind.NumberToken, "0", 0m };
+        yield return new object?[] { SyntaxKind.NumberToken, "1234567890.21", 1234567890.21m };
+        yield return new object?[] { SyntaxKind.QuotationMarksStringToken, "\"ms1\"", "ms1" };
+        yield return new object?[] { SyntaxKind.QuotationMarksStringToken, "\"ms1 ms2\"", "ms1 ms2" };
+        yield return new object?[] { SyntaxKind.SingleQuotationMarksStringToken, "\'ms1\'", "ms1" };
+        yield return new object?[] { SyntaxKind.SingleQuotationMarksStringToken, "\'ms1 ms2\'", "ms1 ms2" };
+    }
+
     [Fact]
     public void Parse_BacktickExpression_With_NameExpression()
     {
