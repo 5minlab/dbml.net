4a9448253d4ad71790427d7743e4c5c35e3da58f
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-17 02:54:29 +0300

    - PASS: `Create_Database_Returns_Column_With_Name_And_Type`

--- tests/DbmlNet.Tests.Unit/Domain/DbmlDatabaseTests.Column.cs
+++ tests/DbmlNet.Tests.Unit/Domain/DbmlDatabaseTests.Column.cs
@@ -8,12 +8,12 @@ namespace DbmlNet.Tests.Unit.Domain;
 public sealed partial class DbmlDatabaseTests
 {
     [Fact]
-    public void Create_Database_With_Table_Column_Name()
+    public void Create_Database_Returns_Column_With_Name_And_Type()
     {
         string text = """
         Table Users
         {
-            id uniqueidentifier
+            Id nvarchar(450)
         }
         """;
         SyntaxTree syntax = SyntaxTree.Parse(text);
@@ -23,7 +23,7 @@ public sealed partial class DbmlDatabaseTests
         Assert.NotNull(database);
         DbmlTable table = Assert.Single(database.Tables);
         DbmlTableColumn column = Assert.Single(table.Columns);
-        Assert.Equal("id", column.Name);
-        Assert.Equal("uniqueidentifier", column.Type);
+        Assert.Equal("Id", column.Name);
+        Assert.Equal("nvarchar(450)", column.Type);
     }
 }
