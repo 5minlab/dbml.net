4147f326c680425921ac58b4404dbc9b0beb8a6a
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-17 02:07:14 +0300

    - PASS: `Create_Database_With_Table_Name_And_Schema`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/DbmlDatabaseTests.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/DbmlDatabaseTests.cs
@@ -169,6 +169,35 @@ public sealed class DbmlDatabaseTests
     }
 
     [Fact]
+    public void Create_Database_With_Table_Name_And_Schema()
+    {
+        string text = """
+        Table identity.Users
+        {
+        }
+        """;
+        SyntaxTree syntax = SyntaxTree.Parse(text);
+
+        DbmlDatabase database = DbmlDatabase.Create(syntax);
+
+        Assert.NotNull(database);
+        Assert.Empty(database.Providers);
+        Assert.Empty(database.Notes);
+        Assert.Empty(database.Note);
+        Assert.Null(database.Project);
+        DbmlTable table = Assert.Single(database.Tables);
+        Assert.Empty(table.Database);
+        Assert.Equal("identity", table.Schema);
+        Assert.Equal("Users", table.Name);
+        Assert.Equal("identity.Users", table.ToString());
+        Assert.Empty(table.Columns);
+        Assert.Empty(table.Indexes);
+        Assert.Empty(table.Relationships);
+        Assert.Empty(table.Notes);
+        Assert.Empty(table.Note);
+    }
+
+    [Fact]
     public void Create_Database_With_Table_Name_And_Schema_And_Database()
     {
         string text = """
