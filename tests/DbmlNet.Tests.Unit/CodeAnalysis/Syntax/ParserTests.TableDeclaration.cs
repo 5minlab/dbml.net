948f244bafd6ef23ad4c6a782757947ef3d7f551
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-17 01:47:25 +0300

    - PASS: `Parse_TableDeclaration_With_Name_And_Schema_Identifier`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.TableDeclaration.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.TableDeclaration.cs
@@ -28,6 +28,33 @@ public partial class ParserTests
     }
 
     [Fact]
+    public void Parse_TableDeclaration_With_Name_And_Schema_Identifier()
+    {
+        SyntaxKind schemaNameKind = SyntaxKind.IdentifierToken;
+        string randomSchemaName = CreateRandomString();
+        string schemaNameText = randomSchemaName;
+        object? schemaNameValue = null;
+        SyntaxKind tableNameKind = SyntaxKind.IdentifierToken;
+        string randomTableName = CreateRandomString();
+        string tableNameText = randomTableName;
+        object? tableNameValue = null;
+        string text = $"Table {schemaNameText}.{tableNameText} " + "{ }";
+
+        MemberSyntax member = ParseMember(text);
+
+        using AssertingEnumerator e = new AssertingEnumerator(member);
+        e.AssertNode(SyntaxKind.TableDeclarationMember);
+        e.AssertToken(SyntaxKind.TableKeyword, "Table");
+        e.AssertNode(SyntaxKind.TableIdentifierClause);
+        e.AssertToken(schemaNameKind, schemaNameText, schemaNameValue);
+        e.AssertToken(SyntaxKind.DotToken, ".");
+        e.AssertToken(tableNameKind, tableNameText, tableNameValue);
+        e.AssertNode(SyntaxKind.BlockStatement);
+        e.AssertToken(SyntaxKind.OpenBraceToken, "{");
+        e.AssertToken(SyntaxKind.CloseBraceToken, "}");
+    }
+
+    [Fact]
     public void Parse_TableDeclaration_With_Empty_Body()
     {
         SyntaxKind tableNameKind = SyntaxKind.IdentifierToken;
