f27661298971e510c9384c0462613acef9573854
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-17 07:03:23 +0300

    - PASS: `Create_Returns_Index_With_Identifier`

--- tests/DbmlNet.Tests.Unit/Domain/DbmlDatabaseTests.Index.cs
+++ tests/DbmlNet.Tests.Unit/Domain/DbmlDatabaseTests.Index.cs
@@ -25,4 +25,36 @@ public sealed partial class DbmlDatabaseTests
         DbmlTable table = Assert.Single(database.Tables);
         Assert.Empty(table.Indexes);
     }
+
+    [Fact]
+    public void Create_Returns_Index_With_Identifier()
+    {
+        string randomIndexName = CreateRandomString();
+        string indexText = $"{randomIndexName}";
+        string text = $$"""
+        Table {{CreateRandomString()}}
+        {
+            Indexes {
+                {{indexText}}
+            }
+        }
+        """;
+        SyntaxTree syntax = ParseNoDiagnostics(text);
+
+        DbmlDatabase database = DbmlDatabase.Create(syntax);
+
+        Assert.NotNull(database);
+        DbmlTable table = Assert.Single(database.Tables);
+        DbmlTableIndex index = Assert.Single(table.Indexes);
+        Assert.Equal(indexText, index.Name);
+        Assert.Equal(indexText, index.ToString());
+        Assert.Equal(indexText, index.ColumnName);
+        Assert.NotNull(index.Table);
+        Assert.Equal(table, index.Table);
+        Assert.False(index.IsPrimaryKey, "Column should not be primary key");
+        Assert.False(index.IsUnique, "Column should not be unique");
+        Assert.Null(index.Type);
+        Assert.Null(index.Note);
+        Assert.Empty(index.Settings);
+    }
 }
