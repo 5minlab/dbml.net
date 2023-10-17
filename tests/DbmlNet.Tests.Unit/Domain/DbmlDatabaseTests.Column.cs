2d5c0e8842a96bd11249eee0294ea69dfa1c7207
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-17 03:06:48 +0300

    - PASS: `Create_Database_Returns_Column_With_Empty_Settings`

--- tests/DbmlNet.Tests.Unit/Domain/DbmlDatabaseTests.Column.cs
+++ tests/DbmlNet.Tests.Unit/Domain/DbmlDatabaseTests.Column.cs
@@ -26,4 +26,36 @@ public sealed partial class DbmlDatabaseTests
         Assert.Equal("Id", column.Name);
         Assert.Equal("nvarchar(450)", column.Type);
     }
+
+    [Fact]
+    public void Create_Database_Returns_Column_With_Empty_Settings()
+    {
+        string text = """
+        Table Users
+        {
+            Id nvarchar(450) [ ]
+        }
+        """;
+        SyntaxTree syntax = SyntaxTree.Parse(text);
+
+        DbmlDatabase database = DbmlDatabase.Create(syntax);
+
+        Assert.NotNull(database);
+        DbmlTable table = Assert.Single(database.Tables);
+        DbmlTableColumn column = Assert.Single(table.Columns);
+        Assert.NotNull(column.Table);
+        Assert.Equal(table, column.Table);
+        Assert.Equal(450, column.MaxLength);
+        Assert.False(column.HasMaxLength, "Column should not have max length");
+        Assert.False(column.IsPrimaryKey, "Column should not be primary key");
+        Assert.False(column.IsUnique, "Column should not be unique");
+        Assert.False(column.IsAutoIncrement, "Column should not be auto increment");
+        Assert.False(column.IsNullable, "Column should not be nullable");
+        Assert.True(column.IsRequired, "Column should be required");
+        Assert.False(column.HasDefaultValue, "Column should not have default value");
+        Assert.Null(column.DefaultValue);
+        Assert.Empty(column.UnknownSettings);
+        Assert.Null(column.Note);
+        Assert.Empty(column.Notes);
+    }
 }
