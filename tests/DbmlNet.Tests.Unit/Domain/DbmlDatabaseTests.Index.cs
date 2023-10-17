d7f1c405236ca39c3e0ae34237542ac174f6de4d
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-17 06:54:32 +0300

    - PASS: `Create_Returns_Index_Empty`

---
+++ tests/DbmlNet.Tests.Unit/Domain/DbmlDatabaseTests.Index.cs
@@ -0,0 +1,28 @@
+using DbmlNet.CodeAnalysis.Syntax;
+using DbmlNet.Domain;
+
+using Xunit;
+
+namespace DbmlNet.Tests.Unit.Domain;
+
+public sealed partial class DbmlDatabaseTests
+{
+    [Fact]
+    public void Create_Returns_Index_Empty()
+    {
+        string text = $$"""
+        Table {{CreateRandomString()}}
+        {
+            Indexes {
+            }
+        }
+        """;
+        SyntaxTree syntax = ParseNoDiagnostics(text);
+
+        DbmlDatabase database = DbmlDatabase.Create(syntax);
+
+        Assert.NotNull(database);
+        DbmlTable table = Assert.Single(database.Tables);
+        Assert.Empty(table.Indexes);
+    }
+}
