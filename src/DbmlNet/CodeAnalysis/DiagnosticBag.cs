b27143b416c7a6b6c5f5ac2afefb340fa17646e2
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-18 04:36:31 +0300

    - Update warning message for unknown index setting

--- src/DbmlNet/CodeAnalysis/DiagnosticBag.cs
+++ src/DbmlNet/CodeAnalysis/DiagnosticBag.cs
@@ -77,7 +77,7 @@ internal sealed class DiagnosticBag : IEnumerable<Diagnostic>
 
     public void ReportUnknownIndexSettingType(TextLocation location, string unknownType)
     {
-        string message = $"Unknown index setting type '{unknownType}'. Allowed type of index ({string.Join(", ", Parser.IndexSettingTypes)} depending on DB).";
+        string message = $"Unknown index setting type '{unknownType}'. Allowed index types: {string.Join(", ", Parser.IndexSettingTypes)}.";
         ReportWarning(location, message);
     }
 }
