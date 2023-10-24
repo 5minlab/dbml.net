30d74f28e1585a932749b6be744d1f71d6811f5f
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-24 03:07:01 +0300

    - PASS: `Parse_RelationshipLongFormDeclaration_With_Relationship`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.RelationshipShortFormDeclaration.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.RelationshipShortFormDeclaration.cs
@@ -144,4 +144,48 @@ public partial class ParserTests
         e.AssertToken(SyntaxKind.OpenBraceToken, "{");
         e.AssertToken(SyntaxKind.CloseBraceToken, "}");
     }
+
+    [Fact]
+    public void Parse_RelationshipLongFormDeclaration_With_Relationship()
+    {
+        // From identifier
+        string fromSchemaText = DataGenerator.CreateRandomString();
+        string fromTableText = DataGenerator.CreateRandomString();
+        string fromColumnText = DataGenerator.CreateRandomString();
+        string fromIdentifierText = $"{fromSchemaText}.{fromTableText}.{fromColumnText}";
+        // To identifier
+        string toSchemaText = DataGenerator.CreateRandomString();
+        string toTableText = DataGenerator.CreateRandomString();
+        string toColumnText = DataGenerator.CreateRandomString();
+        string toIdentifierText = $"{toSchemaText}.{toTableText}.{toColumnText}";
+        // Relationship declaration
+        string text = $$"""
+        Ref
+        {
+            {{fromIdentifierText}} < {{toIdentifierText}}
+        }
+        """;
+
+        MemberSyntax member = ParseMember(text);
+
+        using AssertingEnumerator e = new AssertingEnumerator(member);
+        e.AssertNode(SyntaxKind.RelationshipLongFormDeclarationMember);
+        e.AssertToken(SyntaxKind.RefKeyword, "ref");
+        e.AssertToken(SyntaxKind.OpenBraceToken, "{");
+        e.AssertNode(SyntaxKind.RelationshipConstraintClause);
+        e.AssertNode(SyntaxKind.ColumnIdentifierClause);
+        e.AssertToken(SyntaxKind.IdentifierToken, fromSchemaText);
+        e.AssertToken(SyntaxKind.DotToken, ".");
+        e.AssertToken(SyntaxKind.IdentifierToken, fromTableText);
+        e.AssertToken(SyntaxKind.DotToken, ".");
+        e.AssertToken(SyntaxKind.IdentifierToken, fromColumnText);
+        e.AssertToken(SyntaxKind.LessToken, "<");
+        e.AssertNode(SyntaxKind.ColumnIdentifierClause);
+        e.AssertToken(SyntaxKind.IdentifierToken, toSchemaText);
+        e.AssertToken(SyntaxKind.DotToken, ".");
+        e.AssertToken(SyntaxKind.IdentifierToken, toTableText);
+        e.AssertToken(SyntaxKind.DotToken, ".");
+        e.AssertToken(SyntaxKind.IdentifierToken, toColumnText);
+        e.AssertToken(SyntaxKind.CloseBraceToken, "}");
+    }
 }
