4305db03cf7135dbecd44bcd2f1f7b0a63224618
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-24 21:56:26 +0300

    - PASS `Parse_EnumDeclaration_With_Name_Keyword`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.EnumDeclaration.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.EnumDeclaration.cs
@@ -29,4 +29,29 @@ public partial class ParserTests
         e.AssertToken(SyntaxKind.OpenBraceToken, "{");
         e.AssertToken(SyntaxKind.CloseBraceToken, "}");
     }
+
+    [Theory]
+    [MemberData(nameof(GetSyntaxKeywordTokensData))]
+    public void Parse_EnumDeclaration_With_Name_Keyword(
+        SyntaxKind keywordKind,
+        string keywordText,
+        object? keywordValue)
+    {
+        string text = $$"""
+        enum {{keywordText}} {
+            // no values
+        }
+        """;
+
+        MemberSyntax member = ParseMember(text);
+
+        using AssertingEnumerator e = new AssertingEnumerator(member);
+        e.AssertNode(SyntaxKind.EnumDeclarationMember);
+        e.AssertToken(SyntaxKind.EnumKeyword, "enum");
+        e.AssertNode(SyntaxKind.EnumIdentifierClause);
+        e.AssertToken(keywordKind, keywordText, keywordValue);
+        e.AssertNode(SyntaxKind.BlockStatement);
+        e.AssertToken(SyntaxKind.OpenBraceToken, "{");
+        e.AssertToken(SyntaxKind.CloseBraceToken, "}");
+    }
 }
