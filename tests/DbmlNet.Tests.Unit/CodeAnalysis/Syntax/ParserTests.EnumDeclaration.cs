979a8ba7d00f1709eaba86bda73be33447f5152d
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-24 21:57:02 +0300

    - PASS `Parse_EnumDeclaration_With_Name_And_Schema_Identifier`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.EnumDeclaration.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.EnumDeclaration.cs
@@ -54,4 +54,33 @@ public partial class ParserTests
         e.AssertToken(SyntaxKind.OpenBraceToken, "{");
         e.AssertToken(SyntaxKind.CloseBraceToken, "}");
     }
+
+    [Fact]
+    public void Parse_EnumDeclaration_With_Name_And_Schema_Identifier()
+    {
+        const SyntaxKind schemaNameKind = SyntaxKind.IdentifierToken;
+        string schemaNameText = DataGenerator.CreateRandomString();
+        object? schemaNameValue = null;
+        const SyntaxKind enumNameKind = SyntaxKind.IdentifierToken;
+        string enumNameText = DataGenerator.CreateRandomString();
+        object? enumNameValue = null;
+        string text = $$"""
+        enum {{schemaNameText}}.{{enumNameText}} {
+            // no values
+        }
+        """;
+
+        MemberSyntax member = ParseMember(text);
+
+        using AssertingEnumerator e = new AssertingEnumerator(member);
+        e.AssertNode(SyntaxKind.EnumDeclarationMember);
+        e.AssertToken(SyntaxKind.EnumKeyword, "enum");
+        e.AssertNode(SyntaxKind.EnumIdentifierClause);
+        e.AssertToken(schemaNameKind, schemaNameText, schemaNameValue);
+        e.AssertToken(SyntaxKind.DotToken, ".");
+        e.AssertToken(enumNameKind, enumNameText, enumNameValue);
+        e.AssertNode(SyntaxKind.BlockStatement);
+        e.AssertToken(SyntaxKind.OpenBraceToken, "{");
+        e.AssertToken(SyntaxKind.CloseBraceToken, "}");
+    }
 }
