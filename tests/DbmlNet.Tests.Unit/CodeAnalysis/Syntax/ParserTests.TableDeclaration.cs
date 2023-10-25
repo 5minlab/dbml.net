fbd25d078e36e1b708e2a69b5e10bf65c493ac8a
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-26 01:38:22 +0300

    - PASS: `Parse_TableDeclaration_With_Name_And_Schema_And_Database_Identifier_And_Alias`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.TableDeclaration.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.TableDeclaration.cs
@@ -238,6 +238,42 @@ public partial class ParserTests
         e.AssertToken(SyntaxKind.CloseBraceToken, "}");
     }
 
+    [Fact]
+    public void Parse_TableDeclaration_With_Name_And_Schema_And_Database_Identifier_And_Alias()
+    {
+        const SyntaxKind databaseNameKind = SyntaxKind.IdentifierToken;
+        string databaseNameText = DataGenerator.CreateRandomString();
+        object? databaseNameValue = null;
+        const SyntaxKind schemaNameKind = SyntaxKind.IdentifierToken;
+        string schemaNameText = DataGenerator.CreateRandomString();
+        object? schemaNameValue = null;
+        const SyntaxKind tableNameKind = SyntaxKind.IdentifierToken;
+        string tableNameText = DataGenerator.CreateRandomString();
+        object? tableNameValue = null;
+        const SyntaxKind aliasNameKind = SyntaxKind.IdentifierToken;
+        string aliasNameText = DataGenerator.CreateRandomString();
+        object? aliasNameValue = null;
+        string text = $"Table {databaseNameText}.{schemaNameText}.{tableNameText} as {aliasNameText} " + "{ }";
+
+        MemberSyntax member = ParseMember(text);
+
+        using AssertingEnumerator e = new(member);
+        e.AssertNode(SyntaxKind.TableDeclarationMember);
+        e.AssertToken(SyntaxKind.TableKeyword, "Table");
+        e.AssertNode(SyntaxKind.TableIdentifierClause);
+        e.AssertToken(databaseNameKind, databaseNameText, databaseNameValue);
+        e.AssertToken(SyntaxKind.DotToken, ".");
+        e.AssertToken(schemaNameKind, schemaNameText, schemaNameValue);
+        e.AssertToken(SyntaxKind.DotToken, ".");
+        e.AssertToken(tableNameKind, tableNameText, tableNameValue);
+        e.AssertNode(SyntaxKind.TableAliasClause);
+        e.AssertToken(SyntaxKind.AsKeyword, "as");
+        e.AssertToken(aliasNameKind, aliasNameText, aliasNameValue);
+        e.AssertNode(SyntaxKind.BlockStatement);
+        e.AssertToken(SyntaxKind.OpenBraceToken, "{");
+        e.AssertToken(SyntaxKind.CloseBraceToken, "}");
+    }
+
     [Theory]
     [MemberData(nameof(GetSyntaxKeywordTokensData))]
     public void Parse_TableDeclaration_With_Name_And_Schema_And_Database_Keyword(
