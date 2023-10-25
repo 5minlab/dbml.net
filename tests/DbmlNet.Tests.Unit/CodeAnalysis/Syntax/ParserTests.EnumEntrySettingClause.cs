5a6652b7efb5164432c0c31313ee0d4f35cc96f7
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-25 19:01:22 +0300

    - PASS `Parse_NoteEnumEntrySettingClause_With_SingleQuotationMarksString_Value`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.EnumEntrySettingClause.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.EnumEntrySettingClause.cs
@@ -29,4 +29,28 @@ public partial class ParserTests
         e.AssertToken(settingKind, settingText, settingValue);
         e.AssertToken(SyntaxKind.CloseBracketToken, "]");
     }
+
+    [Fact]
+    public void Parse_NoteEnumEntrySettingClause_With_SingleQuotationMarksString_Value()
+    {
+        const SyntaxKind settingKind = SyntaxKind.SingleQuotationMarksStringToken;
+        string randomText = DataGenerator.CreateRandomMultiWordString();
+        string settingText = $"\'{randomText}\'";
+        object? settingValue = randomText;
+        string text = $$"""
+        {{DataGenerator.CreateRandomString()}} [ note: {{settingText}} ]
+        """;
+
+        EnumEntrySettingListSyntax enumEntrySettingListClause =
+            ParseEnumEntrySettingListClause(text);
+
+        using AssertingEnumerator e = new AssertingEnumerator(enumEntrySettingListClause);
+        e.AssertNode(SyntaxKind.EnumEntrySettingListClause);
+        e.AssertToken(SyntaxKind.OpenBracketToken, "[");
+        e.AssertNode(SyntaxKind.NoteEnumEntrySettingClause);
+        e.AssertToken(SyntaxKind.NoteKeyword, "note");
+        e.AssertToken(SyntaxKind.ColonToken, ":");
+        e.AssertToken(settingKind, settingText, settingValue);
+        e.AssertToken(SyntaxKind.CloseBracketToken, "]");
+    }
 }
