cdf6e1d26ad61573daab0777e67dceb3033bb51f
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-25 19:05:07 +0300

    - PASS `Parse_UnknownEnumEntrySettingClause_With_Composed_Setting_QuotationMarksString_Value`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.EnumEntrySettingClause.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.EnumEntrySettingClause.cs
@@ -109,4 +109,36 @@ public partial class ParserTests
         e.AssertToken(settingValueKind, settingValueText, settingValue);
         e.AssertToken(SyntaxKind.CloseBracketToken, "]");
     }
+
+    [Fact]
+    public void Parse_UnknownEnumEntrySettingClause_With_Composed_Setting_QuotationMarksString_Value()
+    {
+        const SyntaxKind settingNameKind = SyntaxKind.IdentifierToken;
+        string settingNameText = DataGenerator.CreateRandomString();
+        object? settingNameValue = null;
+        const SyntaxKind settingValueKind = SyntaxKind.QuotationMarksStringToken;
+        string randomSettingValue = DataGenerator.CreateRandomMultiWordString();
+        string settingValueText = $"\"{randomSettingValue}\"";
+        object? settingValue = randomSettingValue;
+        string settingText = $"{settingNameText}: {settingValueText}";
+        string text = $$"""
+        {{DataGenerator.CreateRandomString()}} [ {{settingText}} ]
+        """;
+        string[] diagnosticMessages = new[]
+        {
+            $"Unknown enum entry setting '{settingNameText}'.",
+        };
+
+        EnumEntrySettingListSyntax enumEntrySettingListClause =
+            ParseEnumEntrySettingListClause(text, diagnosticMessages);
+
+        using AssertingEnumerator e = new AssertingEnumerator(enumEntrySettingListClause);
+        e.AssertNode(SyntaxKind.EnumEntrySettingListClause);
+        e.AssertToken(SyntaxKind.OpenBracketToken, "[");
+        e.AssertNode(SyntaxKind.UnknownEnumEntrySettingClause);
+        e.AssertToken(settingNameKind, settingNameText, settingNameValue);
+        e.AssertToken(SyntaxKind.ColonToken, ":");
+        e.AssertToken(settingValueKind, settingValueText, settingValue);
+        e.AssertToken(SyntaxKind.CloseBracketToken, "]");
+    }
 }
