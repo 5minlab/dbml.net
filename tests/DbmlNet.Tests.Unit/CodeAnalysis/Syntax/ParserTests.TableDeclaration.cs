75c653fcf5067539f40db347e91b0fd74914c1e1
Catalin-Andronie <catalin.andronie@gmail.com>
2023-10-26 01:35:51 +0300

    - FAIL: `Parse_TableDeclaration_With_Name_Identifier_And_Alias`

--- tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.TableDeclaration.cs
+++ tests/DbmlNet.Tests.Unit/CodeAnalysis/Syntax/ParserTests.TableDeclaration.cs
@@ -26,6 +26,32 @@ public partial class ParserTests
         e.AssertToken(SyntaxKind.CloseBraceToken, "}");
     }
 
+    [Fact]
+    public void Parse_TableDeclaration_With_Name_Identifier_And_Alias()
+    {
+        const SyntaxKind tableNameKind = SyntaxKind.IdentifierToken;
+        string tableNameText = DataGenerator.CreateRandomString();
+        object? tableNameValue = null;
+        const SyntaxKind aliasNameKind = SyntaxKind.IdentifierToken;
+        string aliasNameText = DataGenerator.CreateRandomString();
+        object? aliasNameValue = null;
+        string text = $"Table {tableNameText} as {aliasNameText} " + "{ }";
+
+        MemberSyntax member = ParseMember(text);
+
+        using AssertingEnumerator e = new(member);
+        e.AssertNode(SyntaxKind.TableDeclarationMember);
+        e.AssertToken(SyntaxKind.TableKeyword, "Table");
+        e.AssertNode(SyntaxKind.TableIdentifierClause);
+        e.AssertToken(tableNameKind, tableNameText, tableNameValue);
+        e.AssertNode(SyntaxKind.TableAliasClause);
+        e.AssertToken(SyntaxKind.AsKeyword, "as");
+        e.AssertToken(aliasNameKind, aliasNameText, aliasNameValue);
+        e.AssertNode(SyntaxKind.BlockStatement);
+        e.AssertToken(SyntaxKind.OpenBraceToken, "{");
+        e.AssertToken(SyntaxKind.CloseBraceToken, "}");
+    }
+
     [Theory]
     [MemberData(nameof(GetSyntaxKeywordTokensData))]
     public void Parse_TableDeclaration_With_Name_Keyword(
